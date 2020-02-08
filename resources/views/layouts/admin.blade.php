f699130bf37afdd7f46c9c46005395834a7c7299
susananzth <susananzth@gmail.com>
2020-02-07 23:30:40 -0500

    añadir estilos de login

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -326,10 +326,14 @@
                   Activity Log
                 </a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <a class="dropdown-item" href="{{ route('logout') }}" data-toggle="modal" data-target="#logoutModal" onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Logout
+                  {{ __('Logout') }}
                 </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
               </div>
             </li>
 
