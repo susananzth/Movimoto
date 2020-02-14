ecd686e9317887bf3659dcd6a1f5ac2898f6ec01
Admin-WIN <susananzth@gmail.com>
2020-02-13 21:24:57 -0500

    Agregando título y primera idea del palteamineto del problema

---
+++ resources/views/project/project.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Roboto', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+            .content {
+            }
+            .title {
+                margin: 0.5rem;
+                padding: 0.5rem;
+            }
+            .paragraph {
+                border: #009688 solid;
+                margin: 0.5rem;
+                padding: 0.5rem;
+                text-align: justify;
+            }
+
+        </style>
+    </head>
+    <body>
+            <div class="content">
+                <b class="title">Titulo:</b>
+                <p class="paragraph">
+                    Desarrollo de un sistema web de registro red forzada para empresas y usuarios para
+                    intercambio de productos y servicios en Los Olivos, Lima – Perú
+                </p>
+                <b class="title">Planteamiento del problema</b>
+                <p class="paragraph">
+                    En las provincias de la selva del Perú predomina la movilidad por trimotos,
+                    que son motos lineales modificadas con una tercera rueda en la parte trasera para
+                    una mejor estabilidad, una cabina para la protección del sol y la lluvia y con asiento
+                    para dos pasajeros. La cantidad de trimotos que se ven en las calles aumenta mes a mes,
+                    así como también un gran auge en nuevos comercios mayoristas y minoristas para venta de
+                    repuestos, accesorios, servicios de mantenimiento y revisiones mecánicas, servicios de
+                    limpieza, venta de Seguro Obligatorio de Accidentes de Tránsito (SOAT), entre otros productos
+                    y servicios.
+                    La empresa WIN TECNOLOGIES INC S.A. ha visto una oportunidad para entrar en ese mercado
+                    virgen de tecnología para implantar un sistema que ofrezca orden y una ventana virtual
+                    para que empresas y minoristas puedan ofrecer esos servicios, impulsándose en una red de
+                    confianza a través de redes sociales para su fácil divulgación y no utilizar medios convencionales
+                    y costosos para hacer publicidad a sus productos y servicios.
+Red
+Por lo que necesitan un sistema que pueda registrar cada uno de las empresas y minoristas y puedan ofrecer sus productos y servicios,
+
+
+              </p>
+
+            </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/project', function () {
+    return view('project.project');
+});
 
 
 Auth::routes();
