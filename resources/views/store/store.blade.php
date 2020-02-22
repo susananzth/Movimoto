9b8f66719b478fa38adc2ed882463a45ff82174d
Admin-WIN <susananzth@gmail.com>
2020-02-21 21:27:36 -0500

    Terminado estilos de la vista de la tienda

---
+++ resources/views/store/store.blade.php
@@ -0,0 +1,147 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="store-title">Tienda</h1>
+        <div class="row">
+            <div class="col-md-6 col-xl-4 mb-5">
+                <div class="card post-preview lift h-100">
+                    <img class="card-img-top" src="https://source.unsplash.com/KE0nC8-58MQ/660x360" alt="pic" />
+                    <div class="card-body">
+                        <div class="card-title">Título del producto</div>
+                        <div class="card-text">
+                            <div class="info">Descripción del producto</div>
+                            <div class="style">Color: Rojo / Stilo: M9621C</div>
+                            <div class="price">$50.00 <span class="old-price">$75.00</span></div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="post-preview-meta">
+                            <div class="post-preview-meta-details">
+                              <ul>
+                                <li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
+                              </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-xl-4 mb-5">
+                <div class="card post-preview lift h-100">
+                    <img class="card-img-top" src="https://source.unsplash.com/KE0nC8-58MQ/660x360" alt="pic" />
+                    <div class="card-body">
+                        <div class="card-title">Título del producto</div>
+                        <div class="card-text">
+                            <div class="info">Descripción del producto</div>
+                            <div class="style">Color: Rojo / Stilo: M9621C</div>
+                            <div class="price">$50.00 <span class="old-price">$75.00</span></div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="post-preview-meta">
+                            <div class="post-preview-meta-details">
+                              <ul>
+                                <li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
+                              </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-xl-4 mb-5">
+                <div class="card post-preview lift h-100">
+                    <img class="card-img-top" src="https://source.unsplash.com/KE0nC8-58MQ/660x360" alt="pic" />
+                    <div class="card-body">
+                        <div class="card-title">Título del producto</div>
+                        <div class="card-text">
+                            <div class="info">Descripción del producto</div>
+                            <div class="style">Color: Rojo / Stilo: M9621C</div>
+                            <div class="price">$50.00 <span class="old-price">$75.00</span></div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="post-preview-meta">
+                            <div class="post-preview-meta-details">
+                              <ul>
+                                <li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
+                              </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-xl-4 mb-5">
+                <div class="card post-preview lift h-100">
+                    <img class="card-img-top" src="https://source.unsplash.com/KE0nC8-58MQ/660x360" alt="pic" />
+                    <div class="card-body">
+                        <div class="card-title">Título del producto</div>
+                        <div class="card-text">
+                            <div class="info">Descripción del producto</div>
+                            <div class="style">Color: Rojo / Stilo: M9621C</div>
+                            <div class="price">$50.00 <span class="old-price">$75.00</span></div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="post-preview-meta">
+                            <div class="post-preview-meta-details">
+                              <ul>
+                                <li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
+                              </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-xl-4 mb-5">
+                <div class="card post-preview lift h-100">
+                    <img class="card-img-top" src="https://source.unsplash.com/KE0nC8-58MQ/660x360" alt="pic" />
+                    <div class="card-body">
+                        <div class="card-title">Título del producto</div>
+                        <div class="card-text">
+                            <div class="info">Descripción del producto</div>
+                            <div class="style">Color: Rojo / Stilo: M9621C</div>
+                            <div class="price">$50.00 <span class="old-price">$75.00</span></div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="post-preview-meta">
+                            <div class="post-preview-meta-details">
+                              <ul>
+                                <li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
+                              </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-xl-4 mb-5">
+                <div class="card post-preview lift h-100">
+                    <img class="card-img-top" src="https://source.unsplash.com/KE0nC8-58MQ/660x360" alt="pic" />
+                    <div class="card-body">
+                        <div class="card-title">Título del producto</div>
+                        <div class="card-text">
+                            <div class="info">Descripción del producto</div>
+                            <div class="style">Color: Rojo / Stilo: M9621C</div>
+                            <div class="price">$50.00 <span class="old-price">$75.00</span></div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="post-preview-meta">
+                            <div class="post-preview-meta-details">
+                              <ul>
+                                <li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fa fa-shopping-basket" aria-hidden="true"></i></a></li>
+                              </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,55 +1,44 @@
 <?php
 
 use Illuminate\Routing\Router;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Mail\MailtrapExample;
+use Illuminate\Support\Facades\Mail;
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/project', function () {
-    return view('project.project');
-});
+// Ruta de inicio...
+Route::get('/', function () {return view('index');});
 
+// Ruta del panel administrativo...
+Route::get('/oficina', 'HomeController@index')->name('home');
+
+//Ruta del proyecto escrito...
+Route::get('/project', function () {return view('project.project');});
 
 //Auth::routes();
-// Authentication Routes...
+// Rutas de iniciar sesión...
 Route::get('iniciarsesion', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('iniciarsesion', 'Auth\LoginController@login');
 Route::post('cerrarsesion', 'Auth\LoginController@logout')->name('logout');
 
-// Registration Routes...
+// Rutas de registro...
 Route::get('registrarse', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('registrarse', 'Auth\RegisterController@register');
 
-// Password Reset Routes...
+// Rutas de restablecer contraseña...
 Route::get('contraseña/restablecer', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('contraseña/correo', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('contraseña/restablecer/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('contraseña/restablecer', 'Auth\ResetPasswordController@reset')->name('password.update');
 
-// Password Confirmation Routes...
+// Rutas de confirmar contraseña...
 Route::get('contraseña/confirmar', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
 Route::post('contraseña/confirmar', 'Auth\ConfirmPasswordController@confirm');
 
-// Email Verification Routes...
+// Rutas de verificación de correo...
 Route::get('correo/verificar', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('correo/verificar/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('correo/reenviar', 'Auth\VerificationController@resend')->name('verification.resend');
 
-Route::get('/oficina', 'HomeController@index')->name('home');
-
-use App\Mail\MailtrapExample;
-use Illuminate\Support\Facades\Mail;
-
+//Ruta para enviar correo de prueba...
 Route::get('/send-mail', function () {
 
     Mail::to('newuser@example.com')->send(new MailtrapExample());
@@ -57,3 +46,6 @@ Route::get('/send-mail', function () {
     return 'A message has been sent to Mailtrap!';
 
 });
+
+//Rutas de la tienda...
+Route::get('/tienda', function () {return view('store.store');});
