0eb87502fbb141c6f4a3660102521ff5c16038eb
Admin-WIN <susananzth@gmail.com>
2020-04-13 23:53:42 -0500

    Quitando botón de reportes para clientes

--- resources/views/layouts/customer-admin.blade.php
+++ resources/views/layouts/customer-admin.blade.php
@@ -310,7 +310,6 @@
           <!-- Título -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Tienda</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
           </div>
           <main class="py-4">
               @yield('content')
