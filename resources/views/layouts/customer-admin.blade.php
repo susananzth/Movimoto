607791e872161fbb95042156b87cb3c32b96ae20
Admin-WIN <susananzth@gmail.com>
2020-04-13 23:12:45 -0500

    Configuración del menú lateral para el usuario

--- resources/views/layouts/customer-admin.blade.php
+++ resources/views/layouts/customer-admin.blade.php
@@ -36,113 +36,76 @@
   <!-- Page Wrapper -->
   <div id="wrapper">
 
-    <!-- Sidebar -->
+    <!-- MENÚ LATERAL -->
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-      <!-- Sidebar - Brand -->
+      <!-- Logo -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}"></a>
 
-      <!-- Divider -->
+      <!-- Divisor entre el Logo y el Menú -->
       <hr class="sidebar-divider my-0">
 
-      <!-- Nav Item - Dashboard -->
-      <li class="nav-item active">
-        <a class="nav-link" href="index.html">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Panel</span></a>
+      <!-- Link al dashboard -->
+      <li class="nav-item active store">
+        <a class="nav-link" href="{{ url('/oficina') }}">
+          <i class="fas fa-fw fa-store-alt store"></i>
+          <span class="store">Tienda</span></a>
       </li>
 
-      <!-- Divider -->
-      <hr class="sidebar-divider">
+      <!-- Espacio entre menú
+      <hr class="sidebar-divider">  -->
 
-      <!-- Heading -->
+      <!-- Heading de menú
       <div class="sidebar-heading">
-        Interface
+        Título del menú
       </div>
+      -->
 
-      <!-- Nav Item - Pages Collapse Menu -->
+      <!-- Menú de las ordenes -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          <i class="fas fa-fw fa-cog"></i>
-          <span>Usuarios</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
-          </div>
-        </div>
+        <a class="nav-link" href="{{ url('/oficina') }}">
+          <i class="fas fa-fw fa-list"></i>
+          <span>Ordenes</span></a>
+      </li>
+
+      <!-- Menú del carrito -->
+      <li class="nav-item">
+        <a class="nav-link" href="{{ url('/oficina') }}">
+          <i class="fas fa-fw fa-cart-arrow-down"></i>
+          <span>Carrito</span></a>
+      </li>
+
+      <!-- Menú de la red -->
+      <li class="nav-item">
+        <a class="nav-link" href="{{ url('/oficina') }}">
+          <i class="fas fa-fw fa-network-wired"></i>
+          <span>Red de referidos</span></a>
       </li>
 
       <!-- Nav Item - Utilities Collapse Menu -->
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-          <i class="fas fa-fw fa-wrench"></i>
-          <span>Utilities</span>
+          <i class="fas fa-fw fa-cogs"></i>
+          <span>Configuración</span>
         </a>
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
+            <a class="collapse-item" href="utilities-color.html">Configuración de perfil</a>
+            <a class="collapse-item" href="utilities-border.html">Configuración de pago</a>
+            <a class="collapse-item" href="utilities-animation.html">Configuración de la cuenta</a>
           </div>
         </div>
       </li>
 
-      <!-- Divider -->
-      <hr class="sidebar-divider">
-
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Addons
-      </div>
-
-      <!-- Nav Item - Pages Collapse Menu -->
+      <!-- Menú de ayuda -->
       <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-          <i class="fas fa-fw fa-folder"></i>
-          <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Login Screens:</h6>
-            <a class="collapse-item" href="login.html">Login</a>
-            <a class="collapse-item" href="register.html">Register</a>
-            <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-            <div class="collapse-divider"></div>
-            <h6 class="collapse-header">Other Pages:</h6>
-            <a class="collapse-item" href="404.html">404 Page</a>
-            <a class="collapse-item" href="blank.html">Blank Page</a>
-          </div>
-        </div>
+        <a class="nav-link" href="{{ url('/oficina') }}">
+          <i class="fas fa-fw fa-life-ring"></i>
+          <span>Ayuda</span></a>
       </li>
-
-      <!-- Nav Item - Charts -->
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span></a>
-      </li>
-
-      <!-- Nav Item - Tables -->
-      <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-          <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span></a>
-      </li>
-
-      <!-- Divider -->
-      <hr class="sidebar-divider d-none d-md-block">
-
-      <!-- Sidebar Toggler (Sidebar) -->
-      <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-      </div>
 
     </ul>
-    <!-- End of Sidebar -->
+    <!-- Fin de MENÚ LATERAL -->
 
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
@@ -342,7 +305,7 @@
 
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            <h1 class="h3 mb-0 text-gray-800">Tienda</h1>
             <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
           </div>
           <main class="py-4">
